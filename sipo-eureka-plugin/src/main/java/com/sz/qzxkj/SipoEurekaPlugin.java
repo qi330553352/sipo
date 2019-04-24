295fbe2ccaa187e3fd40c483d40b17724289dc15
qixin <330553352@qq.com>
2019-04-25 00:08:33 +0800

    123

---
+++ sipo-eureka-plugin/src/main/java/com/sz/qzxkj/SipoEurekaPlugin.java
@@ -0,0 +1,15 @@
+package com.sz.qzxkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SipoEurekaPlugin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipoEurekaPlugin.class, args);
+	}
+
+}
