e6b8f0cb4427d9c9e6c05b3e005f640373da6469
qixin <330553352@qq.com>
2019-05-22 00:33:28 +0800

    加强Eureka注册中心的安全认证

---
+++ sipo-eureka-plugin/src/main/java/com/sz/qzxkj/config/SecurityWebConfigurationAdapter.java
@@ -0,0 +1,26 @@
+package com.sz.qzxkj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/** 加强Eureka注册中心的安全认证
+ * 创 建 时 间: 2019/5/21
+ * 版       本: V1.0
+ * 作       者: qixin
+ * 版 权 所 有: 版权所有(C)2019-2029
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityWebConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
